4593f2e996c5ea04c702a3fa7b2f10be2ccfee10
Rochdyath <rochdiath.bachabi@epitech.eu>
2023-04-28 18:06:32 +0100

    feat: relationship between price and revenue

--- suply.py
+++ suply.py
@@ -5,4 +5,13 @@ import plotly.graph_objects as go
 pio.templates.default = "plotly_white"
 
 data = pd.read_csv("supply_chain_data.csv")
-print(data.head())
+
+def relationship_between_price_and_revenue():
+    fig = px.scatter(data, x='Price', 
+                    y='Revenue generated', 
+                    color='Product type', 
+                    hover_data=['Number of products sold'], 
+                    trendline="ols")
+    fig.show()
+
+relationship_between_price_and_revenue()
